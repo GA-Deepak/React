e3e6bf653de74922ad6f1f33e1cd3f248d4895a8
GA-Deepak <deepak.gupta@green-apex.com>
2021-09-24 18:07:04 +0530

    Final commit

--- 23-09-2021/src/App.js
+++ 23-09-2021/src/App.js
@@ -2,18 +2,41 @@ import React, { useState } from "react";
 import "./App.css";
 import MaterialTable from "material-table";
 
+import { confirm } from "react-confirm-box";
+import "reactjs-popup/dist/index.css";
 function App() {
   const [data, setData] = useState([]);
   const columns = [
-    { title: "Firstname", field: "firstname"},
+    { title: "Firstname", field: "firstname" },
     { title: "Lastname", field: "lastname" },
   ];
-
   return (
     <div className="App">
-      <h4 align="center" style={{fontSize:"xx-large"}}>React-Table</h4>
+      <h4 align="center" style={{ fontSize: "xx-large" }}>
+        React-Table
+      </h4>
       <div className="table">
         <MaterialTable
+          actions={[
+            {
+              icon: "delete",
+              tooltip: "Delete User",
+              onClick: async (event, selectedRow) => {
+                const result = await confirm(
+                  "Are you sure you want to delete this?"
+                );
+                if (result) {
+                  new Promise((resolve, reject) => {
+                    const index = selectedRow.tableData.id;
+                    const updatedRows = [...data];
+                    updatedRows.splice(index, 1);
+                    setData(updatedRows);
+                    resolve();
+                  });
+                }
+              },
+            },
+          ]}
           data={data}
           columns={columns}
           editable={{
@@ -28,16 +51,16 @@ function App() {
                   resolve();
                 }, 200);
               }),
-            onRowDelete: (selectedRow) =>
-              new Promise((resolve, reject) => {
-                const index = selectedRow.tableData.id;
-                const updatedRows = [...data];
-                updatedRows.splice(index, 1);
-                setTimeout(() => {
-                  setData(updatedRows);
-                  resolve();
-                }, 200);
-              }),
+            // onRowDelete: (selectedRow) =>
+            //   new Promise((resolve, reject) => {
+            //     const index = selectedRow.tableData.id;
+            //     const updatedRows = [...data];
+            //     updatedRows.splice(index, 1);
+            //     setTimeout(() => {
+            //       setData(updatedRows);
+            //       resolve();
+            //     }, 200);
+            //   }),
             onRowUpdate: (updatedRow, oldRow) =>
               new Promise((resolve, reject) => {
                 const index = oldRow.tableData.id;
@@ -50,7 +73,7 @@ function App() {
               }),
           }}
           options={{
-            showTitle:false,
+            showTitle: false,
             actionsColumnIndex: -1,
             addRowPosition: "first",
           }}
