5943c7f2a3ee2857a6a72b4cd41478744b61ec0f
GA-Deepak <deepak.gupta@green-apex.com>
2021-09-24 12:56:42 +0530

    Final commit

---
+++ 23-09-2021/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./App.css";
+import MaterialTable from "material-table";
+
+function App() {
+  const [data, setData] = useState([]);
+  const columns = [
+    { title: "Firstname", field: "firstname"},
+    { title: "Lastname", field: "lastname" },
+  ];
+
+  return (
+    <div className="App">
+      <h4 align="center" style={{fontSize:"xx-large"}}>React-Table</h4>
+      <div className="table">
+        <MaterialTable
+          data={data}
+          columns={columns}
+          editable={{
+            onRowAdd: (newRow) =>
+              new Promise((resolve, reject) => {
+                const updatedRows = [
+                  ...data,
+                  { id: Math.floor(Math.random() * 100), ...newRow },
+                ];
+                setTimeout(() => {
+                  setData(updatedRows);
+                  resolve();
+                }, 200);
+              }),
+            onRowDelete: (selectedRow) =>
+              new Promise((resolve, reject) => {
+                const index = selectedRow.tableData.id;
+                const updatedRows = [...data];
+                updatedRows.splice(index, 1);
+                setTimeout(() => {
+                  setData(updatedRows);
+                  resolve();
+                }, 200);
+              }),
+            onRowUpdate: (updatedRow, oldRow) =>
+              new Promise((resolve, reject) => {
+                const index = oldRow.tableData.id;
+                const updatedRows = [...data];
+                updatedRows[index] = updatedRow;
+                setTimeout(() => {
+                  setData(updatedRows);
+                  resolve();
+                }, 200);
+              }),
+          }}
+          options={{
+            showTitle:false,
+            actionsColumnIndex: -1,
+            addRowPosition: "first",
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
