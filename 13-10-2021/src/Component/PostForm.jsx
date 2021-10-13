2090ff44b2f2d42af5cc7f696bbd67e33c2f834a
GA-Deepak <deepak.gupta@green-apex.com>
2021-10-13 18:24:54 +0530

    Final commit

--- 13-10-2021/src/Component/PostForm.jsx
+++ 13-10-2021/src/Component/PostForm.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useHistory } from "react-router";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import TextField from '@mui/material/TextField';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import TextField from "@mui/material/TextField";
 const PostForm = () => {
   const [userEmail, setemail] = useState("");
   const [unipsw, setpassword] = useState("");
@@ -19,16 +19,18 @@ const PostForm = () => {
         localStorage.setItem("token", res.data["token"]);
         history.push("/Dashboard");
       })
-      .catch((e) => toast.error('Enter the correct emailid and password', {
-        position: "top-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored"
-        }));
+      .catch((e) =>
+        toast.error("Enter the correct emailid and password", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        })
+      );
   };
 
   const handeleChange1 = (e) => {
@@ -41,23 +43,32 @@ const PostForm = () => {
     <form className="container">
       <h1 style={{ margin: "10px" }}>Login </h1>
       <div>
-      <label htmlFor="id">
-        Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <TextField id="outlined-basic" label="Email" type="email" variant="outlined" required onChange={handeleChange2}/>
-      </label>
+        <TextField
+          id="outlined-basic"
+          label="Email"
+          type="email"
+          variant="outlined"
+          required
+          onChange={handeleChange2}
+        />
       </div>
       <br />
       <div>
-      <label htmlFor="pass">
-        Password:
-        <TextField id="outlined-basic" label="Password" type="password" variant="outlined" required onChange={handeleChange1}/>
-      </label>
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          type="password"
+          variant="outlined"
+          required
+          onChange={handeleChange1}
+        />
       </div>
+      <br />
       <div>
         <Link to="/Dashboard" onClick={f} className="btn btn-primary">
           LogIn
         </Link>
-        <ToastContainer />
+        <ToastContainer/>
       </div>
     </form>
   );
